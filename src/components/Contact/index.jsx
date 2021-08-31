4bbc22156f39ec7b3d09644e2d9520257006b1e6
joaosilva-web <joaogustavosilva2001@gmail.com>
2021-08-31 11:52:18 -0300

    finish the Contact component

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './styles';
 
+import hands from '../../images/hands.png'
+
 export default function Contact() {
+    const [message, setMessage] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setMessage(true);
+    }
     return(
         <S.Contact>
-            
+            <S.ImgContainer>
+                <img src={hands} alt="" />
+            </S.ImgContainer>
+            <S.FormContainer>
+                <h2>Contact.</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="type your Email..."/>
+                    <textarea placeholder="type your message..."></textarea>
+                    <button type="submit">Send</button>
+                    {message && <span>Thanks, I'll reply ASAP :)</span>}
+                </form>
+            </S.FormContainer>
         </S.Contact>
     )
 }
--- src/components/Contact/styles.jsx
+++ src/components/Contact/styles.jsx
@@ -1,5 +1,70 @@
 import styled from "styled-components";
 
 export const Contact = styled.div`
-    
+    display: flex;
+`
+
+export const ImgContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    flex: 1;
+    overflow: hidden;
+
+    img{
+        width: 100%;
+    }
+`
+
+
+export const FormContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    flex: 1;
+
+    h2{
+        font-size:30px;
+    }
+
+    form{
+        width: 70%;
+        height: 50%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-around;
+
+        input{
+            width: 60%;
+            height: 40px;
+            font-size: 18px;
+            padding:0 10px
+        }
+
+        textarea{
+            width: 60%;
+            height: 200px;
+            font-size: 18px;
+            padding:0 10px;
+            resize: none;
+        }
+        button{
+            width: 30%;
+            height: 40px;
+            background: #532DA6;
+            color: #fff;
+
+            border: none;
+            border-radius: 8px;
+            cursor: pointer;
+        }
+
+        span{
+            color: #24A680;
+        }
+    }
 `
